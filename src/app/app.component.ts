5bef1b0e26bb131a6ffb4d8ed4e42cddd196a068
PJ <villanuevapj17@gmail.com>
2024-08-03 16:25:03 +0800

    fxies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MarkdownModule } from 'ngx-markdown';
-import { HttpClientModule } from '@angular/common/http';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MarkdownModule, HttpClientModule],
+  imports: [RouterOutlet, MarkdownModule, HttpClient],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
